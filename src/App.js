f51f0fa134b85dd90a92f1b1f26492a461a51093
allendlg <allen.delagarza01@gmail.com>
2021-09-14 16:40:48 -0500

    day 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import * as ReactBootStrap from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <ReactBootStrap.Container>
+  <ReactBootStrap.Navbar.Brand href="#home">2021 Video Game Guide</ReactBootStrap.Navbar.Brand>
+  <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+    <ReactBootStrap.Nav className="me-auto">
+      <ReactBootStrap.NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <ReactBootStrap.NavDropdown.Item href="#users">Placeholder</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item href="#games">Placeholder</ReactBootStrap.NavDropdown.Item>
+      </ReactBootStrap.NavDropdown>
+    </ReactBootStrap.Nav>
+    <ReactBootStrap.Nav>
+ 
+      <ReactBootStrap.Nav.Link eventKey={2} href="#">
+        Submit review?
+      </ReactBootStrap.Nav.Link>
+    </ReactBootStrap.Nav>
+  </ReactBootStrap.Navbar.Collapse>
+  </ReactBootStrap.Container>
+</ReactBootStrap.Navbar>
     </div>
   );
 }
